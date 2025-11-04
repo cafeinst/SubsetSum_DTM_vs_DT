00cb4240150a03815a003cfecef4da168049ee8e
cafeinst <cafeinst@msn.com>
2025-11-03 20:12:32 -0500

    change at beginning

--- SubsetSum_DTM_vs_DT.thy
+++ SubsetSum_DTM_vs_DT.thy
@@ -1,5 +1,5 @@
 theory SubsetSum_DTM_vs_DT
-  imports "SubsetSum_DTM_Bridge2"
+  imports "SubsetSum_DTM_Bridge3"
 begin
 
 (* ========================================================================= *)
@@ -8,10 +8,10 @@ begin
 
 (* THE FUNDAMENTAL IMPOSSIBILITY RESULT:
    
-   Exponentials beat polynomials! For any polynomial c\<sqdot>n^d, there exists
-   a threshold N such that for all n \<ge> N:
+   Exponentials beat polynomials! For any polynomial c·n^d, there exists
+   a threshold N such that for all n ≥ N:
    
-   \<lceil>c\<sqdot>n^d\<rceil> < 2\<surd>(2^n)
+   ⌈c·n^d⌉ < 2√(2^n)
    
    This is the heart of why subset-sum cannot be in P (under our assumptions).
    The left side grows polynomially; the right side grows exponentially.
@@ -20,39 +20,39 @@ begin
 lemma exp_beats_poly_ceiling_strict_plain:
   fixes c :: real and d :: nat
   assumes cpos: "c > 0"
-  shows "\<exists>N::nat. \<forall>n\<ge>N.
+  shows "∃N::nat. ∀n≥N.
            of_int (ceiling (c * (real n) ^ d)) < 2 * sqrt ((2::real) ^ n)"
 proof -
-  (* Eventually: c * n^d \<le> (\<surd>2)^n *)
-  have ev: "eventually (\<lambda>n. c * (real n) ^ d \<le> (sqrt 2) ^ n) at_top"
+  (* Eventually: c * n^d ≤ (√2)^n *)
+  have ev: "eventually (λn. c * (real n) ^ d ≤ (sqrt 2) ^ n) at_top"
     by real_asymp
   then obtain N1 :: nat where N1:
-    "\<forall>n\<ge>N1. c * (real n) ^ d \<le> (sqrt 2) ^ n"
+    "∀n≥N1. c * (real n) ^ d ≤ (sqrt 2) ^ n"
     by (auto simp: eventually_at_top_linorder)
 
   define N where "N = max N1 1"
 
-  have ceil_le: "of_int (ceiling y) \<le> y + 1" for y :: real
+  have ceil_le: "of_int (ceiling y) ≤ y + 1" for y :: real
     by linarith
 
-  (* Strict step: for n\<ge>1, (\<surd>2)^n + 1 < 2(\<surd>2)^n *)
+  (* Strict step: for n≥1, (√2)^n + 1 < 2(√2)^n *)
   have step_strict:
-    "n \<ge> 1 \<Longrightarrow> (sqrt 2) ^ n + 1 < 2 * (sqrt 2) ^ n"
+    "n ≥ 1 ⟹ (sqrt 2) ^ n + 1 < 2 * (sqrt 2) ^ n"
   proof -
-    assume n1: "n \<ge> 1"
+    assume n1: "n ≥ 1"
     obtain k where nSuc: "n = Suc k" using n1 by (cases n) auto
-    (* (\<surd>2)^k \<ge> 1 *)
-    have pow_ge1: "1 \<le> (sqrt (2::real)) ^ k"
+    (* (√2)^k ≥ 1 *)
+    have pow_ge1: "1 ≤ (sqrt (2::real)) ^ k"
     proof (induction k)
       case 0 show ?case by simp
     next
       case (Suc k)
-      have "1 * sqrt 2 \<le> (sqrt 2) ^ k * sqrt 2"
+      have "1 * sqrt 2 ≤ (sqrt 2) ^ k * sqrt 2"
         using Suc.IH by (simp add: mult_right_mono)
       thus ?case by (smt (verit, del_insts) one_le_power real_sqrt_ge_1_iff)
     qed
-    (* hence \<surd>2 \<le> (\<surd>2)^(Suc k), and since 1 < \<surd>2, we get 1 < (\<surd>2)^n *)
-    have "sqrt 2 \<le> (sqrt 2) ^ Suc k"
+    (* hence √2 ≤ (√2)^(Suc k), and since 1 < √2, we get 1 < (√2)^n *)
+    have "sqrt 2 ≤ (sqrt 2) ^ Suc k"
       using pow_ge1 by (simp add: mult_right_mono)
     hence "1 < (sqrt 2) ^ n"
       using nSuc by (smt (verit) real_sqrt_gt_1_iff)
@@ -61,10 +61,10 @@ proof -
 
   show ?thesis
   proof (rule exI[of _ N], intro allI impI)
-    fix n assume nN: "n \<ge> N"
-    hence nN1: "n \<ge> N1" and n_ge1: "n \<ge> 1" by (auto simp: N_def)
-    from N1 nN1 have bound: "c * (real n) ^ d \<le> (sqrt 2) ^ n" by simp
-    have up: "of_int (ceiling (c * (real n) ^ d)) \<le> (sqrt 2) ^ n + 1"
+    fix n assume nN: "n ≥ N"
+    hence nN1: "n ≥ N1" and n_ge1: "n ≥ 1" by (auto simp: N_def)
+    from N1 nN1 have bound: "c * (real n) ^ d ≤ (sqrt 2) ^ n" by simp
+    have up: "of_int (ceiling (c * (real n) ^ d)) ≤ (sqrt 2) ^ n + 1"
       using ceil_le bound by linarith
     have strict: "(sqrt 2) ^ n + 1 < 2 * (sqrt 2) ^ n"
       using step_strict n_ge1 by auto
@@ -77,7 +77,7 @@ proof -
 qed
 
 (* ========================================================================= *)
-(* PART 2: Simplified TM\<rightarrow>DT Conversion (Cleaner Version)                   *)
+(* PART 2: Simplified TM→DT Conversion (Cleaner Version)                   *)
 (* ========================================================================= *)
 
 context DTM_Run_Sem
@@ -91,7 +91,7 @@ sublocale Base: DTM_Run steps conf head0 accepts .
    This is essentially the same as tm_to_dtr' but with a simpler signature.
    We build it directly into the DTM_Run_Sem context. *)
 
-fun tm_to_dtr :: "nat \<Rightarrow> 'C \<Rightarrow> (nat,nat) dtr" where
+fun tm_to_dtr :: "nat ⇒ 'C ⇒ (nat,nat) dtr" where
   "tm_to_dtr 0 c = Leaf (final_acc c)"
 | "tm_to_dtr (Suc t) c =
      AskL (nat (head0 c))
@@ -130,11 +130,11 @@ next
     have "run ((!) x) ((!) x) (tm_to_dtr (Suc t) (conf M x t0))
           = run ((!) x) ((!) x) (tm_to_dtr t (stepf (conf M x t0) True))"
       by (simp add: True)
-    also have "\<dots> = run ((!) x) ((!) x) (tm_to_dtr t (conf M x (Suc t0)))"
+    also have "… = run ((!) x) ((!) x) (tm_to_dtr t (conf M x (Suc t0)))"
       using True step0 by simp
-    also have "\<dots> = final_acc (conf M x (Suc t0 + t))"
+    also have "… = final_acc (conf M x (Suc t0 + t))"
       by (rule Suc.IH)
-    also have "\<dots> = final_acc (conf M x (t0 + Suc t))"
+    also have "… = final_acc (conf M x (t0 + Suc t))"
       by simp
     finally show ?thesis .
   next
@@ -142,11 +142,11 @@ next
     have "run ((!) x) ((!) x) (tm_to_dtr (Suc t) (conf M x t0))
           = run ((!) x) ((!) x) (tm_to_dtr t (stepf (conf M x t0) False))"
       by (simp add: False)
-    also have "\<dots> = run ((!) x) ((!) x) (tm_to_dtr t (conf M x (Suc t0)))"
+    also have "… = run ((!) x) ((!) x) (tm_to_dtr t (conf M x (Suc t0)))"
       using False step0 by simp
-    also have "\<dots> = final_acc (conf M x (Suc t0 + t))"
+    also have "… = final_acc (conf M x (Suc t0 + t))"
       by (rule Suc.IH)
-    also have "\<dots> = final_acc (conf M x (t0 + Suc t))"
+    also have "… = final_acc (conf M x (t0 + Suc t))"
       by simp
     finally show ?thesis .
   qed
@@ -163,7 +163,7 @@ qed
 
 lemma tm_to_dtr_seen_subset_shift:
   "seenL_run ((!) x) ((!) x) (tm_to_dtr t (conf M x t0))
-   \<subseteq> (\<lambda>u. nat (head0 (conf M x (t0 + u)))) ` {..< t}"
+   ⊆ (λu. nat (head0 (conf M x (t0 + u)))) ` {..< t}"
 proof (induction t arbitrary: t0)
   case 0
   show ?case by simp
@@ -172,7 +172,7 @@ next
   let ?i0 = "nat (head0 (conf M x t0))"
 
   have root_in:
-    "?i0 \<in> (\<lambda>u. nat (head0 (conf M x (t0 + u)))) ` {..< Suc t}"
+    "?i0 ∈ (λu. nat (head0 (conf M x (t0 + u)))) ` {..< Suc t}"
     by (intro image_eqI[where x=0]) (simp_all)
 
   have conf1:
@@ -191,23 +191,23 @@ next
 
   have IH:
     "seenL_run ((!) x) ((!) x) (tm_to_dtr t (conf M x (Suc t0)))
-     \<subseteq> (\<lambda>u. nat (head0 (conf M x (Suc t0 + u)))) ` {..< t}"
+     ⊆ (λu. nat (head0 (conf M x (Suc t0 + u)))) ` {..< t}"
     by (rule Suc.IH)
 
   have shift:
-    "(\<lambda>u. nat (head0 (conf M x (Suc t0 + u)))) ` {..< t}
-    \<subseteq> (\<lambda>v. nat (head0 (conf M x (t0 + v)))) ` {..< Suc t}"
+    "(λu. nat (head0 (conf M x (Suc t0 + u)))) ` {..< t}
+    ⊆ (λv. nat (head0 (conf M x (t0 + v)))) ` {..< Suc t}"
   proof
-    fix y assume "y \<in> (\<lambda>u. nat (head0 (conf M x (Suc t0 + u)))) ` {..< t}"
+    fix y assume "y ∈ (λu. nat (head0 (conf M x (Suc t0 + u)))) ` {..< t}"
     then obtain u where u_lt: "u < t"
       and y_def: "y = nat (head0 (conf M x (Suc t0 + u)))" by auto
-    let ?f = "\<lambda>v. nat (head0 (conf M x (t0 + v)))"
+    let ?f = "λv. nat (head0 (conf M x (t0 + v)))"
     have y_eq: "y = ?f (Suc u)"
       by (simp add: y_def)
-    have Su: "Suc u \<in> {..< Suc t}" using u_lt by simp
-    have "?f (Suc u) \<in> ?f ` {..< Suc t}"
+    have Su: "Suc u ∈ {..< Suc t}" using u_lt by simp
+    have "?f (Suc u) ∈ ?f ` {..< Suc t}"
       using Su by (rule imageI)
-    thus "y \<in> ?f ` {..< Suc t}"
+    thus "y ∈ ?f ` {..< Suc t}"
       by (simp add: y_eq)
   qed
 
@@ -216,20 +216,20 @@ next
        (if x ! ?i0
         then tm_to_dtr t (stepf (conf M x t0) True)
         else tm_to_dtr t (stepf (conf M x t0) False))
-     \<subseteq> (\<lambda>v. nat (head0 (conf M x (t0 + v)))) ` {..< Suc t}"
+     ⊆ (λv. nat (head0 (conf M x (t0 + v)))) ` {..< Suc t}"
     using sub_seen_eq IH shift by blast
 
   show ?case
   proof
     fix i assume i_in:
-      "i \<in> seenL_run ((!) x) ((!) x) (tm_to_dtr (Suc t) (conf M x t0))"
+      "i ∈ seenL_run ((!) x) ((!) x) (tm_to_dtr (Suc t) (conf M x t0))"
     then have "i = ?i0
-               \<or> i \<in> seenL_run ((!) x) ((!) x)
+               ∨ i ∈ seenL_run ((!) x) ((!) x)
                        (if x ! ?i0
                         then tm_to_dtr t (stepf (conf M x t0) True)
                         else tm_to_dtr t (stepf (conf M x t0) False))"
       by simp
-    thus "i \<in> (\<lambda>u. nat (head0 (conf M x (t0 + u)))) ` {..< Suc t}"
+    thus "i ∈ (λu. nat (head0 (conf M x (t0 + u)))) ` {..< Suc t}"
       using root_in subtree_in by blast
   qed
 qed
@@ -246,7 +246,7 @@ lemma tm_to_dtr_correct:
 (* THEOREM: The tree only queries positions that the TM reads *)
 lemma tm_to_dtr_seen_subset_read0:
   "seenL_run ((!) x) ((!) x) (tm_to_dtr (steps M x) (conf M x 0))
-   \<subseteq> Base.read0 M x"
+   ⊆ Base.read0 M x"
   using tm_to_dtr_seen_subset_shift[of x "steps M x" 0]
   by (simp add: Base.read0_def)
 
@@ -265,8 +265,8 @@ end  (* context DTM_Run_Sem *)
 locale DTM_refines_DTR = DTM_Run_Sem +
   fixes n :: nat
   assumes head_in_range:
-    "\<And>x t. length x = n \<Longrightarrow> t < steps M x
-           \<Longrightarrow> 0 \<le> head0 (conf M x t) \<and> nat (head0 (conf M x t)) < n"
+    "⋀x t. length x = n ⟹ t < steps M x
+           ⟹ 0 ≤ head0 (conf M x t) ∧ nat (head0 (conf M x t)) < n"
  (* If the input has length n and we haven't halted yet,
        the head is in range [0, n) *)
 
@@ -281,7 +281,7 @@ lemma dt_steps_on_x_eq_tm_steps:
   (* The tree makes exactly (steps M x) queries *)
 
 lemma dt_correct_on_x:
-  "run (\<lambda>i. x ! i) (\<lambda>j. x ! j)
+  "run (λi. x ! i) (λj. x ! j)
        (tm_to_dtr (steps M x) (conf M x 0)) = accepts M x"
   by (rule tm_to_dtr_correct)
 
@@ -296,12 +296,12 @@ begin
 
 (* LEMMA 1 (Formalized): There exists an instance where T must read all blocks *)
 lemma lemma1_adversarial_lower_bound:
-  assumes n_ge2: "n \<ge> 2"
-      and kk_bounds: "1 \<le> kk" "kk < n"
-  shows "\<exists>as s. 
-           length as = n \<and> 
-           distinct_subset_sums as \<and>
-           steps M (enc as s kk) \<ge> 
+  assumes n_ge2: "n ≥ 2"
+      and kk_bounds: "1 ≤ kk" "kk < n"
+  shows "∃as s. 
+           length as = n ∧ 
+           distinct_subset_sums as ∧
+           steps M (enc as s kk) ≥ 
              card (LHS (e_k as s kk) n) + card (RHS (e_k as s kk) n)"
 proof -
   (* Use pow2_list *)
@@ -313,7 +313,7 @@ proof -
     by (simp add: as_def distinct_subset_sums_pow2_list)
   
   (* Apply steps_lower_bound - NOW with as_def and s_def *)
-  have "steps M (enc as s kk) \<ge> card (LHS (e_k as s kk) n) + card (RHS (e_k as s kk) n)"
+  have "steps M (enc as s kk) ≥ card (LHS (e_k as s kk) n) + card (RHS (e_k as s kk) n)"
     using steps_lower_bound[OF len_as[symmetric] dist_as n_ge2 kk_bounds as_def s_def] .
   
   thus ?thesis using len_as dist_as by blast
@@ -324,32 +324,32 @@ qed
 (* ========================================================================= *)
 
 theorem exists_hard_instance_exponential_lower_bound:
-  assumes n_ge2: "n \<ge> 2"
-      and kk_bounds: "1 \<le> kk" "kk < n"
-  shows "\<exists>as s.
-           length as = n \<and>
-           distinct_subset_sums as \<and>
-           real (steps M (enc as s kk)) \<ge> 2 * sqrt ((2::real) ^ n)"
+  assumes n_ge2: "n ≥ 2"
+      and kk_bounds: "1 ≤ kk" "kk < n"
+  shows "∃as s.
+           length as = n ∧
+           distinct_subset_sums as ∧
+           real (steps M (enc as s kk)) ≥ 2 * sqrt ((2::real) ^ n)"
 proof -
-  have kk_le_n: "kk \<le> n" using kk_bounds by simp
+  have kk_le_n: "kk ≤ n" using kk_bounds by simp
   
   (* Lemma 1: There exists an instance where T reads all blocks *)
   obtain as s where
     len: "length as = n" and
     dist: "distinct_subset_sums as" and
-    reads_all: "steps M (enc as s kk) \<ge> 
+    reads_all: "steps M (enc as s kk) ≥ 
                   card (LHS (e_k as s kk) n) + card (RHS (e_k as s kk) n)"
     using lemma1_adversarial_lower_bound[OF n_ge2 kk_bounds] by blast
   
-  (* Lemma 2 & 3: |LHS| + |RHS| \<ge> 2\<surd>(2^n) by AM-GM *)
+  (* Lemma 2 & 3: |LHS| + |RHS| ≥ 2√(2^n) by AM-GM *)
   have "real (card (LHS (e_k as s kk) n) + card (RHS (e_k as s kk) n))
-        \<ge> 2 * sqrt ((2::real) ^ n)"
+        ≥ 2 * sqrt ((2::real) ^ n)"
     using lhs_rhs_sum_lower_bound[OF len[symmetric] kk_le_n dist] .
   
   (* Combine *)
-  have "real (steps M (enc as s kk)) \<ge> 2 * sqrt ((2::real) ^ n)"
-    using reads_all \<open>2 * sqrt (2 ^ n) \<le> real (card (LHS (e_k as s kk) n) + 
-          card (RHS (e_k as s kk) n))\<close> by linarith
+  have "real (steps M (enc as s kk)) ≥ 2 * sqrt ((2::real) ^ n)"
+    using reads_all ‹2 * sqrt (2 ^ n) ≤ real (card (LHS (e_k as s kk) n) + 
+          card (RHS (e_k as s kk) n))› by linarith
   
   thus ?thesis using len dist by blast
 qed
@@ -359,31 +359,31 @@ qed
 (* ========================================================================= *)
 
 theorem no_polytime_on_worst_case:
-  assumes kk_bounds: "1 \<le> kk" 
-      and kk_always_strict: "\<And>n. n \<ge> 2 \<Longrightarrow> kk < n"
-  shows "\<not> (\<exists>(c::real)>0. \<exists>(d::nat).
-           \<forall>n\<ge>2. \<forall>as s. length as = n \<and> distinct_subset_sums as \<longrightarrow>
-             real (steps M (enc as s kk)) \<le> c * real n ^ d)"
+  assumes kk_bounds: "1 ≤ kk" 
+      and kk_always_strict: "⋀n. n ≥ 2 ⟹ kk < n"
+  shows "¬ (∃(c::real)>0. ∃(d::nat).
+           ∀n≥2. ∀as s. length as = n ∧ distinct_subset_sums as ⟶
+             real (steps M (enc as s kk)) ≤ c * real n ^ d)"
 proof
-  assume poly: "\<exists>(c::real)>0. \<exists>(d::nat).
-                 \<forall>n\<ge>2. \<forall>as s. length as = n \<and> distinct_subset_sums as \<longrightarrow>
-                   real (steps M (enc as s kk)) \<le> c * real n ^ d"
+  assume poly: "∃(c::real)>0. ∃(d::nat).
+                 ∀n≥2. ∀as s. length as = n ∧ distinct_subset_sums as ⟶
+                   real (steps M (enc as s kk)) ≤ c * real n ^ d"
   
   obtain c d where c_pos: "c > 0" and
-    UB: "\<forall>n\<ge>2. \<forall>as s. length as = n \<and> distinct_subset_sums as \<longrightarrow>
-                  real (steps M (enc as s kk)) \<le> c * real n ^ d"
+    UB: "∀n≥2. ∀as s. length as = n ∧ distinct_subset_sums as ⟶
+                  real (steps M (enc as s kk)) ≤ c * real n ^ d"
     using poly by blast
   
   (* Exponentials beat polynomials *)
-  obtain N where Nbig_ceil: "\<forall>n\<ge>N. of_int \<lceil>c * real n ^ d\<rceil> < 2 * sqrt ((2::real)^n)"
+  obtain N where Nbig_ceil: "∀n≥N. of_int ⌈c * real n ^ d⌉ < 2 * sqrt ((2::real)^n)"
     using exp_beats_poly_ceiling_strict_plain[OF c_pos] by blast
   
-  have Nbig: "\<forall>n\<ge>N. c * real n ^ d < 2 * sqrt ((2::real)^n)"
+  have Nbig: "∀n≥N. c * real n ^ d < 2 * sqrt ((2::real)^n)"
   proof (intro allI impI)
-    fix n assume "n \<ge> N"
-    then have "of_int \<lceil>c * real n ^ d\<rceil> < 2 * sqrt ((2::real)^n)"
+    fix n assume "n ≥ N"
+    then have "of_int ⌈c * real n ^ d⌉ < 2 * sqrt ((2::real)^n)"
       using Nbig_ceil by simp
-    moreover have "c * real n ^ d \<le> of_int \<lceil>c * real n ^ d\<rceil>"
+    moreover have "c * real n ^ d ≤ of_int ⌈c * real n ^ d⌉"
       by linarith
     ultimately show "c * real n ^ d < 2 * sqrt ((2::real)^n)"
       by linarith
@@ -391,32 +391,32 @@ proof
   
   (* Pick n large enough *)
   define n where "n = max N 2"
-  have n_geN: "n \<ge> N" and n_ge2: "n \<ge> 2" by (simp_all add: n_def)
+  have n_geN: "n ≥ N" and n_ge2: "n ≥ 2" by (simp_all add: n_def)
   
   (* kk bounds for this specific n *)
   have kk_lt_n: "kk < n"
     using kk_always_strict[OF n_ge2] .
   
-  have kk_bounds_n: "1 \<le> kk" "kk < n"
+  have kk_bounds_n: "1 ≤ kk" "kk < n"
     using kk_bounds kk_lt_n by simp_all
   
   (* Get the hard instance (pow2_list) *)
   obtain as s where
     len: "length as = n" and
     dist: "distinct_subset_sums as" and
-    LB: "real (steps M (enc as s kk)) \<ge> 2 * sqrt ((2::real)^n)"
+    LB: "real (steps M (enc as s kk)) ≥ 2 * sqrt ((2::real)^n)"
     using exists_hard_instance_exponential_lower_bound[OF n_ge2 kk_bounds_n] 
     by blast
   
   (* Upper bound from polynomial assumption applied to THIS instance *)
-  have UB_inst: "real (steps M (enc as s kk)) \<le> c * real n ^ d"
+  have UB_inst: "real (steps M (enc as s kk)) ≤ c * real n ^ d"
     using UB[rule_format, OF n_ge2, of as s] len dist by blast
   
   (* Lower bound from exponential *)
   have LT: "c * real n ^ d < 2 * sqrt ((2::real)^n)"
     using Nbig n_geN by simp
   
-  (* Contradiction: steps \<ge> 2\<surd>(2^n) > c\<sqdot>n^d \<ge> steps *)
+  (* Contradiction: steps ≥ 2√(2^n) > c·n^d ≥ steps *)
   from LB LT UB_inst show False by linarith
 qed
 
